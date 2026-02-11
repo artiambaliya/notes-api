24f2c3891c2a2b72acebc541a03bd6f0e431640d
artiambaliya <ambaliyaarti397@gmail.com>
2026-02-11 15:04:53 +0530

    add auth controller file

---
+++ controller/authController.js
@@ -0,0 +1,40 @@
+const User = require("../models/userModel");
+
+const registerUser = async(req, res, next) => {
+
+    try{
+        const { name, email, password } = req.body
+
+        if(!name || !email || !password){
+            const error = new Error("All fildes are required");
+            error.status = 400;
+            throw error;
+        }
+
+        const existinUser = await User.findOne({ email })
+
+        if(!existinUser){
+            const error = new Error("User alreay exists");
+            error.status = 400;
+            throw error;
+        }
+
+        const user = await User.create({
+            name, 
+            email,
+            password,
+        });
+
+        return res.status(201).json({
+            id : user._id,
+            name : user.name,
+            email : user.email,
+        });
+        
+    }catch(err){
+        next(err);
+    }
+}
+
+
+module.exports = { registerUser }
