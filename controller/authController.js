1791e93e034bd0c83135484590b67189e7ca0134
artiambaliya <ambaliyaarti397@gmail.com>
2026-02-13 16:44:51 +0530

    add login system

--- controller/authController.js
+++ controller/authController.js
@@ -3,10 +3,10 @@ const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res, next) => {
 
-    try{
+    try {
         const { name, email, password } = req.body
 
-        if(!name || !email || !password){
+        if (!name || !email || !password) {
             const error = new Error("All fildes are required");
             error.status = 400;
             throw error;
@@ -14,7 +14,7 @@ const registerUser = async (req, res, next) => {
 
         const existingUser = await User.findOne({ email })
 
-        if(existingUser){
+        if (existingUser) {
             const error = new Error("User alreay exists");
             error.status = 400;
             throw error;
@@ -26,21 +26,62 @@ const registerUser = async (req, res, next) => {
         const hashPassword = await bcrypt.hash(password, salt);
 
         const user = await User.create({
-            name, 
+            name,
             email,
             password: hashPassword,
         });
 
         return res.status(201).json({
-            id : user._id,
-            name : user.name,
-            email : user.email,
+            id: user._id,
+            name: user.name,
+            email: user.email,
         });
 
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 }
 
 
-module.exports = { registerUser }
+const loginUser =  async(req, res, next) => {
+
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        const error = new Error("email and password required");
+        error.status = 400;
+        throw error;
+    }
+
+    const user = await User.findOne({ email });
+
+    if(!user){
+        const error = new Error("invalid credantials");
+        error.status = 404;
+        throw error;
+    }
+
+    //compare password
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        const error = new Error("invalid credantials");
+        error.staus = 401;
+        throw error;
+    }
+
+    return res.status(200).json({
+        id: user._id,
+        name: user.name,
+        email: user.email
+    });
+
+}
+
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controller/authController");
+const {
+    registerUser,
+    loginUser } = require("../controller/authController");
 
 
 router.post("/register", registerUser);
 
+router.post("/login", loginUser);
 
-module.exports  = router;
+module.exports = router;
