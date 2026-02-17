5b97ca1748f504f2c3931c42a9626018fc5c4cdb
artiambaliya <ambaliyaarti397@gmail.com>
2026-02-17 16:59:40 +0530

    generate token

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+
 
 const registerUser = async (req, res, next) => {
 
@@ -71,13 +73,22 @@ const loginUser =  async(req, res, next) => {
         throw error;
     }
 
+    // generate token
+
+    const token = jwt.sign(
+        {id: user._id},
+        process.env.JWT_SECRET, 
+        {expiresIn : "1d"}
+    );
+
     return res.status(200).json({
         id: user._id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        token
     });
 
-    
+
 
 }
 
